87a141f348868e71ae8e4230bef668a73470d476
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2014-09-15 14:27:28 +0800

    navbar: sign up, sign in, facebook

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,16 +1,15 @@
 class MealsController < ApplicationController
-  #before_action :authenticate_user!, except:[:index, :show]
+  before_action :authenticate_user!, except:[:index, :show]
+  before_action :find_meal, only:[:show, :edit, :update, :destroy]
   def index
     @meals = Meal.all
   end
   def show
-    @meal = Meal.find(params[:id])
   end
   def new
     @meal = Meal.new
   end
   def edit
-    @meal = Meal.find(params[:id])
   end
   def create
     @meal = Meal.new(meal_params)
@@ -21,7 +20,6 @@ class MealsController < ApplicationController
     end
   end
   def update
-    @meal = Meal.find(params[:id])
     if @meal.update(meal_params)
       redirect_to meals_path, notice:"修改成功"
     else
@@ -29,11 +27,13 @@ class MealsController < ApplicationController
     end
   end
   def destroy
-    @meal = Meal.find(params[:id])
     @meal.destroy
       redirect_to meals_path, alert:"餐點已取消"
   end
   private
+  def find_meal
+    @meal = Meal.find(params[:id])
+  end
   def meal_params
     params.require(:meal).permit(:title, :takeType, :price, :address, :menu, 
       :minGuests, :maxGuests, :startTime, :endTime, :picture)
