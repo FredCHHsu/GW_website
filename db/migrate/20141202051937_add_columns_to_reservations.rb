6f5ef515fa788f2d576ee0a29e982bf85bfda52f
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2014-12-08 16:17:51 +0800

    fix migration bug Reservations->reservation

--- db/migrate/20141202051937_add_columns_to_reservations.rb
+++ db/migrate/20141202051937_add_columns_to_reservations.rb
@@ -1,11 +1,11 @@
 class AddColumnsToReservations < ActiveRecord::Migration
   def change
-    add_column :Reservations, :payment, :boolean, default: false
-    add_column :Reservations, :amount, :integer, default: 1
-    add_column :Reservations, :totalprice, :integer
-    add_column :Reservations, :contact_name, :string
-    add_column :Reservations, :contact_mail, :string
-    add_column :Reservations, :contact_phone, :integer
+    add_column :reservations, :payment, :boolean, default: false
+    add_column :reservations, :amount, :integer, default: 1
+    add_column :reservations, :totalprice, :integer
+    add_column :reservations, :contact_name, :string
+    add_column :reservations, :contact_mail, :string
+    add_column :reservations, :contact_phone, :integer
 
   end
 end
