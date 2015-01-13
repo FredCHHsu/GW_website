0c66a86f81853688105fecd5c44f87f3519affc0
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2015-01-13 18:42:49 +0800

    add mina repository setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,18 +2,18 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-set :user, 'azureuser'
+
 set :domain, 'guestwhat-t01.cloudapp.net'
 set :deploy_to, '/var/www/guestwhat-t01.com'
-#set :repository, 'git@github.com:FredCHHsu/GW_website.git'
-#set :branch, 'master'
+set :repository, 'https://github.com/FredCHHsu/GW_website'
+set :branch, 'master'
 
 # For system-wide RVM install.
 #   set :rvm_path, '/usr/local/rvm/bin/rvm'
@@ -23,9 +23,9 @@ set :deploy_to, '/var/www/guestwhat-t01.com'
 set :shared_paths, ['config/database.yml', 'log']
 
 # Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+  set :user, 'azureuser'    # Username in the server to SSH to.
+  set :port, '22'     # SSH port number.
+  set :forward_agent, true     # SSH forward_agent.
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
@@ -35,7 +35,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rvm:use[ruby-2.1.5@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
